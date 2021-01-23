2975a4b419eddd57daba36c51d9755977d0c480e
mohamed lamine <mohamed61lamine@gmail.com>
2021-01-23 10:45:30 +0100

    Animated Hamburger Menu

--- app/js/script.js
+++ app/js/script.js
@@ -1,1 +1,14 @@
-console.log('hello world');
+const btnHamburger = document.querySelector('#btnHamburger');
+const header = document.querySelector('.header');
+
+btnHamburger.addEventListener('click', function () {
+    console.log('click Hamburger');
+
+    if(header.classList.contains('open')){
+        header.classList.remove('open');
+    }
+    else {
+        header.classList.add('open');
+    }
+    
+});
