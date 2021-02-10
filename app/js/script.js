c0fcff6060244c2e7c5621ca0b006e15624041e8
laminegaci <mohamed61lamine@gmail.com>
2021-02-10 12:06:57 +0100

    Animated Mobile Menu ( 100% )

--- app/js/script.js
+++ app/js/script.js
@@ -1,19 +1,29 @@
 const btnHamburger = document.querySelector('#btnHamburger');
 const header = document.querySelector('.header');
 const overlay = document.querySelector('.overlay');
+const fadeElements = document.querySelectorAll('.has-fade');
+const body = document.querySelector('body');
 
 btnHamburger.addEventListener('click', function () {
     console.log('click Hamburger');
 
     if (header.classList.contains('open')) {
+        body.classList.remove('noscroll');
         header.classList.remove('open');
-        overlay.classList.remove('fade-in');
-        overlay.classList.add('fade-out');
+        fadeElements.forEach(function (element) {
+            element.classList.remove('fade-in');
+            element.classList.add('fade-out');
+        });
+
     }
     else {
+        body.classList.add('noscroll');
         header.classList.add('open');
-        overlay.classList.add('fade-in');
-        overlay.classList.remove('fade-out');
+        fadeElements.forEach(function (element) {
+            element.classList.add('fade-in');
+            element.classList.remove('fade-out');
+        });
+
     }
 
 });
